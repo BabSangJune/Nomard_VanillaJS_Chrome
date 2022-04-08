eecb99c351377c812bf939510799f1fa171a4acb
Sang June Park <73651893+BabSangJune@users.noreply.github.com>
2022-04-08 12:19:08 +0900

    2022.04.08_4.7
    2022.04.08_4.7

---
+++ test.js
@@ -0,0 +1,11 @@
+var string = 'hello World'
+var search = 'l';
+var count = 0;
+
+for (var i = 0; i < string.length; i++) {
+  if (string[i] !== search) continue;
+
+  count++;
+}
+
+console.log(count) 
